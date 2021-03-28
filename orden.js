66249fbed64062f8d805c277ab62c3cc2d51e3bb
ThomasNass <tn900323@gmail.com>
2021-03-28 10:43:59 +0200

    Copy pasted Array
    Found out that you need a server to fetch a JSON-file so I just copy pasted the array straight into the code... I know, this is not the way to go, but I will fix it when I have more knowledge.

--- orden.js
+++ orden.js
@@ -2,13 +2,19 @@
 
 // const ordenspråken = document.querySelectorAll('i')[0].innerText;
 
-let ordenspråken = [].map.call(document.querySelectorAll('i'), function(el) {
-    return el.innerText;
-});
+// let ordenspråken = [].map.call(document.querySelectorAll('i'), function(el) {
+//     return el.innerText;
+// });
 
-console.log(ordenspråken)
+// console.log(ordenspråken)
 
 // ordArr = Array.from(ordenspråken);
 // let string = JSON.stringify(ordenspråken);
 // console.log(JSON.stringify(ordenspråken));
 // console.log(JSON.parse(string))
+
+fetch("./idioms.json")
+.then(response =>{
+return response.json();
+})
+.then(data => console.log(data));
